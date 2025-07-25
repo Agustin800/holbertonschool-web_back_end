5a7d5beda8098414d3275518c8df52387dd6277f
Agustin800 <aguslahalo@gmail.com>
2025-07-25 04:35:58 +0000

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+'''funcion que redondea para abajo'''
+
+
+import math
+
+def floor(n: float) -> int:
+    '''retorna el entero mas chico disponible'''
+    return math.floor(n)
