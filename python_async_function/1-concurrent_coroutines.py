bf793577f455e447a2ca489ae88d1ec339d71a9c
Agustin800 <aguslahalo@gmail.com>
2025-07-26 14:25:05 +0000

    task 1 cambio de importacion y tasks por confuncion y lo sobre escrbia xd

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,13 +6,13 @@ los tiempos ordenados.'''
 
 import asyncio
 from typing import List
-from 0-basic_async_syntax.py import wait_random
+from basic_async_syntax import wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    '''c+retorna la lista con todas las esperas dentro'''
+    '''retorna la lista con todas las esperas dentro'''
     delays = []
-    task = [wait_random(max_delay) for a in range(n)]
+    tasks = [wait_random(max_delay) for a in range(n)]
 
     for task in asyncio.as_completed(task):
         delay = await task
