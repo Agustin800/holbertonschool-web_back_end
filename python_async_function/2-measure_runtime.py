ca2bb429e9b034e269f23256a61cede6d4617dff
Agustin800 <aguslahalo@gmail.com>
2025-07-28 05:05:25 +0000

    task 2, nombre correcto de la funcion de la importacion

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -6,14 +6,14 @@
 import asyncio
 import time
 import importlib
-wait = importlib.import_module('1-concurrent_coroutines').wait
+wait_n = importlib.import_module('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
     '''usa time para medir el intervalo de ejecucion usando
     el reloj de alta resolucion'''
     start = time.perf_counter()
-    asyncio.run(wait(n, max_delay))
+    asyncio.run(wait_n(n, max_delay))
     end = time.perf_counter()
     total = start - end
     return total / n
