9e9565a040fcc5880660e05bef5eb8b446109801
Agustin800 <aguslahalo@gmail.com>
2025-07-29 03:50:54 +0000

    task 2, pycodestyle

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -6,11 +6,14 @@ async_comprehension 4 veces en paralelo."""
 import asyncio
 import time
 import importlib
-async_comprehension = importlib.import_module('1-async_comprehension').async_comprehension
+async_comprehension = (
+    importlib.import_module('1-async_comprehension').async_comprehension
+)
 
 
 async def measure_runtime() -> float:
-    '''ejecuta async_comprehension 4 veces en paralelo y mide el tiempo total.'''
+    '''ejecuta async_comprehension 4 veces en paralelo
+    y mide el tiempo total.'''
     start = time.perf_counter()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end = time.perf_counter()
