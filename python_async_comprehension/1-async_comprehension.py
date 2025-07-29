d4361f7f4981869b402764bedf8aa6ff658c4bc9
Agustin800 <aguslahalo@gmail.com>
2025-07-29 03:39:24 +0000

    task 1, bien importado

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,7 @@ from typing import List
 import asyncio
 import importlib
 from typing import Generator
-async_generator = importlib.import_module('0-asinc_generator').async_generator
+async_generator = importlib.import_module('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     '''recopila 10 números aleatorios desde async_generator.'''
