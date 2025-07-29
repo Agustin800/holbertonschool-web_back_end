60696d4a18d960b6685590677c487243f061e16a
Agustin800 <aguslahalo@gmail.com>
2025-07-29 03:32:44 +0000

    task 0, le agrego la anotacion de tipo correcta

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,9 +5,10 @@
 
 import random
 import asyncio
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     '''genera 10 números aleatorios entre 0 y 10
     con una pausa de 1 segundo.'''
     for _ in range(10):
