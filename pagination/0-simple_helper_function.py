b3ae1df4244873b4606d76f6bcfa3e3cbb57000b
Agustin800 <aguslahalo@gmail.com>
2025-08-14 13:48:01 +0000

    documentacion

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-'''funcion que espera un tiempo entre 0 y 10'''
+'''funcion que para calcular el rango de índices para paginación'''
 
 
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''Calcula el índice inicial y final para obtener una página de elementos
+    de una lista'''
     start = (page - 1) * page_size
     end = page * page_size
     return start, end
