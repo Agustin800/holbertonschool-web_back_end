b259e73f78e06e4a949ee23e8dc5a2670acb17af
Agustin800 <aguslahalo@gmail.com>
2025-08-17 18:29:14 +0000

    task 2, ahora si, cambio de identacion  y retorna un diccionario y no lista

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -4,7 +4,7 @@
 
 import csv
 import math
-from typing import List
+from typing import List, Dict
 import importlib
 index_range = importlib.import_module('0-simple_helper_function').index_range
 
@@ -26,30 +26,30 @@ class Server:
             self.__dataset = dataset[1:]  # quitar cabecera
         return self.__dataset
 
-def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        '''Retorna una pagina del dataset'''
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            '''Retorna una pagina del dataset'''
+            assert isinstance(page, int) and page > 0
+            assert isinstance(page_size, int) and page_size > 0
 
-        start, end = index_range(page, page_size)
+            start, end = index_range(page, page_size)
 
-        dataset = self.dataset()
+            dataset = self.dataset()
 
-        if start >= len(dataset):
-            return []
-        return dataset[start:end]
+            if start >= len(dataset):
+                return []
+            return dataset[start:end]
 
-def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
-    '''Retorna una página del dataset'''
-    data = self.ger_page(page, page_size)
-    all_items = len(self.dataset())
-    total_pages = math.ceil(all_items / page_size)
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        '''Retorna una página del dataset'''
+        data = self.get_page(page, page_size)
+        all_items = len(self.dataset())
+        total_pages = math.ceil(all_items / page_size)
 
-    return{
-         'page_size': len(data),
-         'page': page,
-         'data': data,
-         'next_page': page + 1 if page < total_pages else None,
-         'pre_page': page - 1 if page > 1 else None,
-         'total_pages': total_pages
-    }
+        return{
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if page < total_pages else None,
+            'pre_page': page - 1 if page > 1 else None,
+            'total_pages': total_pages
+        }
