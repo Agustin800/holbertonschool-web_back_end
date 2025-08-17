09a0fa0e20cdfb10abd87d491ec02610afc13872
Agustin800 <aguslahalo@gmail.com>
2025-08-17 19:15:58 +0000

    task 10, pycodestyle

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-'''Funcion que cambie todos los temas (topics) de un
+'''Funcion que cambia todos los temas (topics) de un
 documento de la colección school según el nombre'''
 
 
 def update_topics(mongo_collection, name, topics):
+    '''cambia todos los temas (topics) de un
+    documento de la colección school según el nombre'''
     mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
-    
