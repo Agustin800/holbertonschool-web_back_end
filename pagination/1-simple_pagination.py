e056ccc5408a11cef9c53f0ac650a94d541befa3
Agustin800 <aguslahalo@gmail.com>
2025-08-17 18:16:58 +0000

    task 1, pycodestyle

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -29,14 +29,14 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            '''Retorna una pagina del dataset'''
-            assert isinstance(page, int) and page > 0
-            assert isinstance(page_size, int) and page_size > 0
+        '''Retorna una pagina del dataset'''
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
-            start, end = index_range(page, page_size)
+        start, end = index_range(page, page_size)
 
-            dataset = self.dataset()
+        dataset = self.dataset()
 
-            if start >= len(dataset):
-                 return []
-            return dataset[start:end]
+        if start >= len(dataset):
+            return []
+        return dataset[start:end]
