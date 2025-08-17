d1f2903088ec7b13dfdd145931b51fa85f345840
Agustin800 <aguslahalo@gmail.com>
2025-08-17 18:35:39 +0000

    task 3

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Clase Server para paginar una base de datos de nombres de bebés."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Devuelve el dataset en caché."""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Devuelve el dataset indexado por posición, empezando en 0."""
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        '''Retorna una página del dataset resistente a eliminaciones.'''
+        dataset = self.indexed_dataset()
+        total_items = len(dataset)
+
+        assert 0 <= index < total_items, f"Index {index} is out of range"
+
+        page_data = []
+        current_index = index
+
+        while len(page_data) < page_size:
+            if dataset.get(current_index) is not None:
+                page_data.append(dataset[current_index])
+            current_index += 1
+
+        next_index = current_index
+
+        return {
+            'index': index,
+            'next_index': next_index,
+            'page_size': len(page_data),
+            'data': page_data
+        }
